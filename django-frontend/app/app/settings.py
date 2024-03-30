8c7a9ac936afd15fee11fcd63735f1004b417e41
Teddy <yddetco@gmail.com>
2024-03-29 21:28:35 -0400

    DATABASE:CONFIG: Add postgres container & configuration for frontend

--- django-frontend/app/app/settings.py
+++ django-frontend/app/app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'database',
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
