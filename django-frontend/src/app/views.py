c5057332dc59f43e9d51801b2958981b1de9ab90
gvial15 <guillaumevial15@gmail.com>
2024-04-21 18:20:20 -0400

    some adjustment

--- django-frontend/src/app/views.py
+++ django-frontend/src/app/views.py
@@ -4,47 +4,27 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 
+fake_account_info = {
+    'username': 'fake_username',
+    'email': 'fake_email'
+}
+
 def index(request):
-    if request.user.is_authenticated:
+    # if user authenticated
         return render(request, 'index.html')
-    else:
-        return auth(request) # TEMPORARY AUTH using django built in auth
+    # else:
+    #     return auth(request)
 
 def sidebar(request):
-    account_info = {
-        'username': request.user.username,
-        'email': request.user.email
-    }
-    return  render(request, 'sidebar.html', account_info)
+    return  render(request, 'sidebar.html', fake_account_info)
 
 def game(request):
     return  render(request, 'game.html')
 
-# TEMPORARY AUTH using django built in auth for testing purpose
 # THIS FUNCTION WILL MAKE CALLS TO THE DRF-API-GATEWAY AND/OR DRF-API-AUTH
 def auth(request):
-    # Handle registration
-    if request.method == 'POST' and 'register' in request.POST:
-        email = request.POST.get('emailInput')
-        password = request.POST.get('passwordInput')
-        username = request.POST.get('usernameInput')
-        try:
-            user = User.objects.create_user(username=username, email=email, password=password)
-            login(request, user)
-            return redirect('index')
-        except IntegrityError:
-            messages.error(request, 'That email is already taken. Please choose another one.')
-    # Handle login
-    elif request.method == 'POST' and 'login' in request.POST:
-        username = request.POST.get('usernameInput')
-        password = request.POST.get('passwordInput')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            messages.error(request, 'Invalid email or password')
-    return render(request, 'auth.html')
+        return
+
 
 # TEMPORARY LOGOUT for testing purpose
 def logout_view(request):
