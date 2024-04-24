59ad9d36eda42d87c4753d4610170bcd8536088d
gvial15 <guillaumevial15@gmail.com>
2024-04-24 15:58:10 -0400

    sidebar friend lists titles styling

--- django-frontend/src/app/static/js/sidebar.js
+++ django-frontend/src/app/static/js/sidebar.js
@@ -1,24 +1,47 @@
-// Show and hide friend list content
 function handleFriendListClick() {
-	let element = document.getElementById('friend-list-title');
-    element.onclick = function() {
-		console.log("friend list title clicked")
+    let title = document.getElementById('friend-list-title');
+    title.onclick = function() {
+        let content = document.getElementById('friend-list-content');
+        let computedDisplay = window.getComputedStyle(content).getPropertyValue('display');
+        if (computedDisplay === 'none') {
+			document.getElementById('friend-list-title-arrow').innerHTML = "&#x25BC;";
+            content.style.display = 'block';
+        } else {
+			document.getElementById('friend-list-title-arrow').innerHTML = "&#9654;";
+			content.style.display = 'none';
+        }
     };
 }
 
 // Show and hide friend requests list content
 function handleFriendRequestsListClick() {
-	let element = document.getElementById('friend-requests-list-title');
-    element.onclick = function() {
-		console.log("friend requests list title clicked")
+    let title = document.getElementById('friend-requests-list-title');
+    title.onclick = function() {
+        let content = document.getElementById('friend-requests-list-content');
+        let computedDisplay = window.getComputedStyle(content).getPropertyValue('display');
+        if (computedDisplay === 'none') {
+			document.getElementById('friend-requests-list-title-arrow').innerHTML = "&#x25BC;";
+            content.style.display = 'block';
+        } else {
+			document.getElementById('friend-requests-list-title-arrow').innerHTML = "&#9654;";
+            content.style.display = 'none';
+        }
     };
 }
 
 // Show and hide add friend list content
 function handleAddFriendListClick() {
-	let element = document.getElementById('add-friend-list-title');
-    element.onclick = function() {
-        console.log("add friend list title clicked")
+	let title = document.getElementById('add-friend-list-title');
+    title.onclick = function() {
+		let content = document.getElementById('add-friend-list-content');
+        let computedDisplay = window.getComputedStyle(content).getPropertyValue('display');
+        if (computedDisplay === 'none') {
+			document.getElementById('add-friend-list-title-arrow').innerHTML = "&#x25BC;";
+			content.style.display = 'block';
+        } else {
+			document.getElementById('add-friend-list-title-arrow').innerHTML = "&#9654;";
+			content.style.display = 'none';
+        }
     };
 }
 
