11cd30b4806f49c8f3f34a77ee9969c472ad8939
ASAEL <pokemoninja@hotmail.ca>
2024-04-30 18:04:53 -0400

    game/templates/game/ added

--- django-frontend/src/game/views.py
+++ django-frontend/src/game/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def game(request: HttpRequest) -> HttpResponse :
-    return (render(request, 'pong_game.html'))
+    return (render(request, 'game/pong_game.html'))
 
