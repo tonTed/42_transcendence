31ca02f95a3698c2f4ce20f300babed2f5994fa1
gvial15 <guillaumevial15@gmail.com>
2024-05-10 21:25:23 -0400

    remove all the fetch and use django {% include template.html  %} and remove all fetch js functions, urls and views

--- django-frontend/src/frontend/urls.py
+++ django-frontend/src/frontend/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-from frontend.views import index, topbar, sidebar, chat, profile, game, gateway
+from frontend.views import index, game, gateway
 
 urlpatterns = [
 	path('', index, name='index'),
-	path('topbar/', topbar, name='topbar'),
-	path('sidebar/', sidebar, name='sidebar'),
-	path('chat/', chat, name='chat'),
-
-	# Profile
-	path('profile/', profile, name='profile'),
-
 
 	path('game/', game, name='game'),
 	path('gateway/', gateway, name='gateway'),
--- django-frontend/src/frontend/views.py
+++ django-frontend/src/frontend/views.py
@@ -22,44 +22,6 @@ def login(request):
     render(request, 'login.html')
 
 
-def topbar(request):
-    user: dict = api.gateway.get_user_info(request.session['token42'])
-    context: dict = {
-        'user': user,
-    }
-    return render(request, 'topbar.html', context)
-
-
-def sidebar(request):
-    friends: list[dict] = api.gateway.get_friends()
-    friends_requests: list[dict] = api.gateway.get_friends_requests()
-    friends_add: list[dict] = api.gateway.get_friends_add()
-
-    context = {
-        'friends': friends,
-        'friends_add': friends_add,
-        'friend_requests': friends_requests,
-    }
-    return render(request, 'sidebar.html', context)
-
-
-def chat(request):
-    mock_global_chat_messages: list[dict] = api.gateway.get_mock_global_chat_messages()
-    context = {
-        'mock_global_chat_messages': mock_global_chat_messages,
-    }
-    return render(request, 'chat.html', context)
-
-
-# Profile
-def profile(request):
-    user: dict = api.gateway.get_user_info(request.session['token42'])    
-    context: dict = {
-        'user': user,
-    }
-    return render(request, 'profile.html', context)
-
-
 # Game
 def game(request):
     return render(request, 'game.html')
