b44606ad290b6243ff7ac73bf8b9e774c8e7220a
Teddy <yddetco@gmail.com>
2024-06-11 18:27:14 -0400

    Form for create password if user not exists

--- django-frontend/src/authentication/urls.py
+++ django-frontend/src/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import login, callback, logout
+from authentication.views import login, callback, logout, create_password
 
 
 urlpatterns = [
     path('frontend/login/', login, name='login'),
     path('callback/', callback, name='callback'),
     path('logout/', logout, name='logout'),
+    path('frontend/create_password/', create_password, name='create_password'),
 ]
--- django-frontend/src/authentication/views.py
+++ django-frontend/src/authentication/views.py
@@ -35,11 +35,12 @@ def callback(request) -> HttpResponse:
     user = requests.get(f'{API_URL}/users/get_user_info_with_id_42/{me["id_42"]}')
 
     if user.status_code == 404:
-        user = requests.post(f'{API_URL}/users/create_user/', json=me)
+        # Rediriger vers une page pour créer un mot de passe
+        request.session['me'] = me  # Stocker les données utilisateur dans la session
+        return redirect('create_password')
 
-    if user.json()['is_2fa_enabled']:
-        return redirect('login_password', permanent=True)
-    
+    # if user.json()['is_2fa_enabled']:
+
     jwt_token = requests.post(f'{API_URL}/auth/generate/', json={'user_id': user.json()['id']})
 
     response: HttpResponse = redirect(f"/")
@@ -52,3 +53,23 @@ def logout(request: HttpRequest) -> HttpResponse:
     response: HttpResponse = redirect(f"/")
     response.delete_cookie('jwt_token')
     return response
+
+
+def create_password(request):
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        me = request.session.get('me')
+        me['password'] = password
+        user_response = requests.post(f'{API_URL}/users/create_user/', json=me)
+        
+        if user_response.status_code == 201:
+            user = user_response.json()
+            jwt_token = requests.post(f'{API_URL}/auth/generate/', json={'user_id': user['id']})
+            
+            response = redirect('/')
+            response.set_cookie('jwt_token', jwt_token.json()['access'])
+            return response
+        else:
+            return render(request, 'create_password.html', {'error': 'Failed to create user'})
+    
+    return render(request, 'create_password.html')
