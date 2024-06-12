6a1a4153ade88b61af1773b9938e6e376c0da691
Teddy <yddetco@gmail.com>
2024-06-12 14:59:04 -0400

    Password hased and username unique

--- drf-api-users/src/users/models.py
+++ drf-api-users/src/users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 
-# TODO: add password field hashed
 class User(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     avatar_url = models.CharField(max_length=255, blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     email = models.EmailField()
@@ -11,6 +11,11 @@ class User(models.Model):
     is_2fa_enabled = models.BooleanField(default=False)
     id_42 = models.CharField(max_length=42)
     friends = models.ManyToManyField('self', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk or 'password' in kwargs.get('update_fields', []):
+            self.password = make_password(self.password)
+        super(User, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.username
