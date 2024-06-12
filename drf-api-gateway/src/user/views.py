b773ae93219dc9d1d6bb1d1290f4401c8dff8af5
Teddy <yddetco@gmail.com>
2024-06-12 08:10:38 -0400

    Add urls services in .env

--- drf-api-gateway/src/user/views.py
+++ drf-api-gateway/src/user/views.py
@@ -1,13 +1,14 @@
-import requests
+import os
 from django.http import HttpResponse
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_protect
+import requests
 from liveUpdate.decorators import refresh_live_update
 import jwt
 
+USER_URL = os.getenv('USER_URL')
 
-# TODO: Add URL to user service in .env and const at beginning of file
 # TODO: Create middleware to check if user is authorized and ignore some routes (like login)
 # TODO: Remove id from path and use jwt token to get user id
 # TODO: Manage if error occurs in requests
@@ -16,19 +17,19 @@ import jwt
 
 @api_view(['GET'])
 def users(request):
-    response = requests.get('http://api-users:3001/users/', json=request.data)
+    response = requests.get(f'{USER_URL}/')
     return HttpResponse(response.content, status=response.status_code, content_type=request.content_type)
 
 
 @api_view(['GET'])
 def user_info(request, id):
-    response = requests.get(f'http://api-users:3001/users/{id}')
+    response = requests.get(f'{USER_URL}/{id}')
     return HttpResponse(response.content, status=response.status_code, content_type=request.content_type)
 
 
 @api_view(['POST'])
 def create_user(request):
-    response = requests.post('http://api-users:3001/users/', json=request.data)
+    response = requests.post(f'{USER_URL}/', json=request.data)
     return HttpResponse(response.content, status=response.status_code, content_type=request.content_type)
 
 
@@ -39,7 +40,7 @@ def updateUsername(request, user_id):
     jwt_token = request.COOKIES.get('jwt_token')
     payload = jwt.decode(jwt_token, options={"verify_signature": False}, algorithms=["none"])
     user_id = payload['user_id']
-    response = requests.put(f'http://api-users:3001/users/{user_id}', data=request.body,
+    response = requests.put(f'{USER_URL}/{user_id}', data=request.body,
                             headers={'Content-Type': request.content_type})
     return HttpResponse(response.content, status=response.status_code, content_type=request.content_type)
 
@@ -50,14 +51,14 @@ def updateAvatar(request, user_id):
     jwt_token = request.COOKIES.get('jwt_token')
     payload = jwt.decode(jwt_token, options={"verify_signature": False}, algorithms=["none"])
     user_id = payload['user_id']
-    response = requests.put(f'http://api-users:3001/users/{user_id}', data=request.body,
+    response = requests.put(f'{USER_URL}/{user_id}', data=request.body,
                             headers={'Content-Type': request.content_type})
     return HttpResponse(response.content, status=response.status_code, content_type=request.content_type)
 
 
 @api_view(['GET'])
 def get_user_info_with_id_42(request, id_42):
-    response = requests.get(f'http://api-users:3001/users/get_user_info_with_id_42/{id_42}')
+    response = requests.get(f'{USER_URL}/get_user_info_with_id_42/{id_42}')
     if response.status_code == 200:
         return JsonResponse(response.json(), status=200)
     else:
