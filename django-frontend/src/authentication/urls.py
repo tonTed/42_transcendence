159ae6a62b7a2f7925a10e4dd978377d26fe44bd
tonTed <yddetco@gmail.com>
2024-09-04 19:24:37 -0400

    FIX: set online first connection and when 2fa & Flake8 to Auth

--- django-frontend/src/authentication/urls.py
+++ django-frontend/src/authentication/urls.py
@@ -3,9 +3,9 @@ from authentication.views import login, callback, logout, create_password, verif
 
 
 urlpatterns = [
-    path('frontend/login/', login, name='login'),
-    path('callback/', callback, name='callback'),
-    path('logout/', logout, name='logout'),
-    path('frontend/create_password/', create_password, name='create_password'),
-    path('frontend/verify_2fa/', verify_2fa, name='verify_2fa'),
+    path("frontend/login/", login, name="login"),
+    path("callback/", callback, name="callback"),
+    path("logout/", logout, name="logout"),
+    path("frontend/create_password/", create_password, name="create_password"),
+    path("frontend/verify_2fa/", verify_2fa, name="verify_2fa"),
 ]
