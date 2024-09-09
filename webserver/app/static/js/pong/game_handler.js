7558044871142e9a7b8ab31bea5722ffe3e73868
Teddy Blanco <tblanco@c1r4p3.42quebec.com>
2024-09-09 13:46:15 -0400

    fix maj key

--- webserver/app/static/js/pong/game_handler.js
+++ webserver/app/static/js/pong/game_handler.js
@@ -1,58 +1,59 @@
 export const gameState = {
-    actions: {
-        p1Up: false,
-        p1Down: false,
-        p2Up: false,
-        p2Down: false,
-    },
-    gameStarted: false,
-    players: {
-        p1Name: null,
-        p2Name: null
-    },
-    paused: false
+  actions: {
+    p1Up: false,
+    p1Down: false,
+    p2Up: false,
+    p2Down: false,
+  },
+  gameStarted: false,
+  players: {
+    p1Name: null,
+    p2Name: null,
+  },
+  paused: false,
 };
 
 const keyToActionMap = {
-    'w': 'p1Up',
-    's': 'p1Down',
-    'ArrowUp': 'p2Up',
-    'ArrowDown': 'p2Down'
+  w: "p1Up",
+  s: "p1Down",
+  W: "p1Up",
+  S: "p1Down",
+  ArrowUp: "p2Up",
+  ArrowDown: "p2Down",
 };
 
 function handleKeyDown(event) {
-    if (event.key == 'Escape'){
-        gameState.paused = !gameState.paused;
-        return;
+  if (event.key == "Escape") {
+    gameState.paused = !gameState.paused;
+    return;
+  }
+  if (!gameState.paused) {
+    const action = keyToActionMap[event.key];
+    if (action) {
+      gameState.actions[action] = true;
     }
-    if (!gameState.paused){
-        const action = keyToActionMap[event.key];
-        if (action) {
-            gameState.actions[action] = true;
-        }
-    }
+  }
 }
 
 function handleKeyUp(event) {
-    const action = keyToActionMap[event.key];
-    if (action) {
-        gameState.actions[action] = false;
-    }
+  const action = keyToActionMap[event.key];
+  if (action) {
+    gameState.actions[action] = false;
+  }
 }
 
 export function handlerGameLoop() {
+  document.removeEventListener("keydown", handleKeyDown);
+  document.removeEventListener("keyup", handleKeyUp);
 
-    document.removeEventListener('keydown', handleKeyDown);
-    document.removeEventListener('keyup', handleKeyUp);
-
-    document.addEventListener('keydown', handleKeyDown);
-    document.addEventListener('keyup', handleKeyUp);
+  document.addEventListener("keydown", handleKeyDown);
+  document.addEventListener("keyup", handleKeyUp);
 }
 
-export function gameIsInPlay(gameData){
-    return (!gameData.resetting && !gameData.winner);
+export function gameIsInPlay(gameData) {
+  return !gameData.resetting && !gameData.winner;
 }
 
-export function winnerIsDecided(gameData){
-    return (gameData.winner);
+export function winnerIsDecided(gameData) {
+  return gameData.winner;
 }
